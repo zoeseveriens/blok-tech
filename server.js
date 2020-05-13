d91c8c0f963eb77e2fef62fe792872711874ead1
zoeseveriens <zoe.222@hotmail.com>
2020-05-13 16:12:14 +0200

    HW week2

---
+++ server.js
@@ -0,0 +1,17 @@
+
+var express = require('express')
+
+express()
+    .use(express.static('static'))
+    .get('/', movies)
+    .get('/about', about)
+    .listen(8000)
+
+    function movies (req, res) {
+        res.status(200).send('server says hello')
+    }
+
+    function about(req, res){
+        res.status(200).send('about page')
+    }
+
