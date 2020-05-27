3af20be1369d1feab4cd586db61a0f2d1c773642
zoeseveriens <zoe.222@hotmail.com>
2020-05-27 19:25:22 +0200

    test

--- server.js
+++ server.js
@@ -6,21 +6,21 @@ const ejsLint = require('ejs-lint')
 const slug = require('slug')
 const bodyParser = require('body-parser')
 
-const matchesData = [
+
+const data = [
     {
         id: 'shane',
-        img: 'no img',
         name: 'Shane',
         age: '24',
         description:'Love to travel',
     },
     {
-        id: 'chicky-nuggies',
-        img: 'no img',
-        name: 'Chicky Nuggies',
-        age: '1',
-        description:'Baby Yoda trying to say vegetables',
+        id: 'daniel',
+        name: 'Daniel',
+        age: '22',
+        description:'...',
     }
+
 ]
 
 express()
@@ -28,18 +28,35 @@ express()
     .use(bodyParser.urlencoded({extended:true}))
     .set('view engine', 'ejs') //express gaat de viewengine ejs vanaf nu gebruiken
     .set('views', 'view')  //al mijn views staan in het mapje view
-    .get('/', forYou)
+    .get('/foryou', forYou)
+    .post('/likes', add)
     .get('/likes', likes)
     .get('/about', about)
     .get('/404', notFound)
-    .listen(7000)
+    .listen(8000)
 
-    function forYou (req, res) {
-        res.status(200).render('static/index.html')
+    function forYou (req, res){
+        res.render('foryou.ejs')
     }
 
+    function add(req, res) {
+        console.log(req.body.name);
+        var id = slug(req.body.name).toLowerCase()
+
+      
+        data.push({
+          id: id,
+          name: req.body.name,
+          age: req.body.age,
+          description: req.body.description
+        })
+      
+        res.redirect('/' + id)
+        
+      }
+
     function likes (req, res){
-        res.render('likes.ejs', {data: matchesData})
+        res.render('likes.ejs', {data: data})
     }
 
     function about(req, res){
