dd940e6d720f4f8e0abbbda4b6fc1d05bb5635da
zoeseveriens <zoe.222@hotmail.com>
2020-06-04 16:05:23 +0200

    worked on feature

--- model/your_profile.js
+++ model/your_profile.js
@@ -19,6 +19,7 @@ const UserInterests = mongoose.model('UserInterests', UserInterestsSchema);
 module.exports = UserInterests;
 
 //Creating an instance of the model user interests
+// moet in de server.js
 const UserInterestsList = new UserInterests({Interests : 'Skydiving'}); //dit is wat je ziet op je account page
 
 
